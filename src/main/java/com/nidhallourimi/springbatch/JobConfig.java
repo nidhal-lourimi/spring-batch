69b49531bce77317ee6a7ca2ecf6e886e577c3c4
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-08-21 14:56:52 +0100

    building a batch job

---
+++ src/main/java/com/nidhallourimi/springbatch/JobConfig.java
@@ -0,0 +1,15 @@
+package com.nidhallourimi.springbatch;
+
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*@Configuration
+public class JobConfig {
+
+    @Bean
+    JobBuilderFactory jobBuilderFactory(){
+        return   new JobBuilderFactory();
+    }
+}*/
